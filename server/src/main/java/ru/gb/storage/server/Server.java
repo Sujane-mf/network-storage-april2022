3f3933930001a8264d5c655d0541f8dda765ada7
sujane <sujane_mf@mail.ru>
2022-04-25 11:18:52 +0300

    1) Добавлен простой сервер с первого урока по Netty 2) Добавлен простой клиент с первого урока по Netty 3) Добавлено открытие окна при подключении клиента

--- server/src/main/java/ru/gb/storage/server/Server.java
+++ server/src/main/java/ru/gb/storage/server/Server.java
@@ -1,7 +1,80 @@
 package ru.gb.storage.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
 public class Server {
-    public static void main(String[] args) {
-        System.out.println("Server started");
+    private final int port;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Server(8189).start();
+    }
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try{
+            ServerBootstrap server = new ServerBootstrap();
+            server
+                    .group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                            nioSocketChannel.pipeline().addLast(
+                                    new LineBasedFrameDecoder(256),
+                                    new StringDecoder(),
+                                    new StringEncoder(),
+                                    new SimpleChannelInboundHandler<String>() {
+                                        @Override
+                                        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+                                            System.out.println("Канал зарегистрирован");
+                                        }
+
+                                        @Override
+                                        public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+                                            System.out.println("Канал разрегистрирован");
+                                        }
+
+                                        @Override
+                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                            System.out.println("Канал активирован");
+                                        }
+
+                                        @Override
+                                        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                                            System.out.println("Канал деактивирован");
+                                        }
+
+                                        @Override
+                                        protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+                                            ctx.writeAndFlush(msg);
+                                        }
+                                    }
+                            );
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture future = server.bind(port).sync();
+            System.out.println("Server started");
+            future.channel().closeFuture().sync();
+
+        }
+        finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
